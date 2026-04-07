e2209e0b2fe4a4133ae723db053c1adabf6d008f
Florian <12160@holbertonstudents.com>
2026-04-07 13:50:02 +0200

    Add to_kv function with type annotations

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List, Union
+
+"""Module for to_str function with type annotations."""
+
+
+def to_kv(k
+    """Return the sum of a list of floats."""
+    total = sum(mxd_lst)
+    return total
