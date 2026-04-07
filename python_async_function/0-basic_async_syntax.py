52e3bb2f9971ba5160898d50debea70fe7568df7
Florian <12160@holbertonstudents.com>
2026-04-07 15:21:18 +0200

    Remove unused main function and related imports from wait_random module

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,15 +10,3 @@ async def wait_random(max_delay: int = 10) -> float:
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-#!/usr/bin/env python3
-import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-async def main():
-    print(await wait_random())
-    print(await wait_random(5))
-    print(await wait_random(15))
-
-if __name__ == "__main__":
-    asyncio.run(main())
