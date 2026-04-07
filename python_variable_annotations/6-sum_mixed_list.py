cc01d69a6f855a57250b8538263fc565ad3932ab
Florian <12160@holbertonstudents.com>
2026-04-07 13:52:26 +0200

    Refactor sum_mixed_list function to remove unused import and ensure proper type hinting

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-from typing import List, Union
+from typing import List
+
 
 """Module for to_str function with type annotations."""
 
 
-def to_kv(k
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
     """Return the sum of a list of floats."""
     total = sum(mxd_lst)
     return total
