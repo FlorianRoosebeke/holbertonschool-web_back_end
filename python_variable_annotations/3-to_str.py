e7a74ea862c575c1de3955f580c1a91bc8faa91e
Florian <12160@holbertonstudents.com>
2026-04-07 11:45:02 +0200

    Add to_str function with type annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+"""Module for to_str function with type annotations."""
+
+
+def to_str(n: float) -> str:
+    """Return the str of a float."""
+    result = str(n)
+    return result
