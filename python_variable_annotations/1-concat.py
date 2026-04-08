327ef18a99368aeb1209ca506787e9e8debf742c
Florian <12160@holbertonstudents.com>
2026-04-08 10:46:53 +0200

    Fix formatting by removing extra whitespace in concat function

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module for concat function with type annotations."""
-    
+
 
 def concat(str1: str, str2: str) -> str:
     """Return the concatenation of two strings."""
