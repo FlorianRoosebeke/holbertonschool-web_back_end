81da6bb1cf7e76b6ea8e2ee0ab629c95b92450fa
Florian <12160@holbertonstudents.com>
2026-04-08 10:50:14 +0200

    Refactor floor function to use direct import from math module and simplify return statement

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-import math
-
 """Module for floor function with type annotations."""
+from math import floor as math_floor
 
 
 def floor(n: float) -> int:
-    """Return the floor of a float."""
-    result = math.floor(n)
-    return result
+    """Return the floor of n."""
+    return math_floor(n)
