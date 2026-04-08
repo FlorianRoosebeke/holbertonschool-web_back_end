d36a31cbf33dda6df96907aa07fac2bf831715ae
Florian <12160@holbertonstudents.com>
2026-04-08 10:58:34 +0200

    Fix module docstring placement in 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""Module for make_multiplier function with type annotations."""
+
 from typing import Callable
 
 
-"""Module for make_multiplier function with type annotations."""
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
