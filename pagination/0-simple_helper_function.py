b2403e45fc9b99e1e8db45f183c0cbcd4a1a8944
Florian <12160@holbertonstudents.com>
2026-04-21 12:50:29 +0200

    Refactor return statement in index_range function for clarity

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,4 +2,4 @@ def index_range(page, page_size) -> tuple[int, int]:
     """Return the start and end indexes for a pagination page."""
     start = (page - 1) * page_size
     end = start + page_size
-    return start, end
+    return (start, end)
