7bda7b1ba078b369148305c0fe0aa86b9cd48fa1
Florian <12160@holbertonstudents.com>
2026-04-21 12:45:17 +0200

    Add pagination helper function and update README with learning objectives

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,5 @@
+def index_range(page, page_size) -> tuple[int, int]:
+    """Return the start and end indexes for a pagination page."""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
