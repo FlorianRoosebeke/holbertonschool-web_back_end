0a54a18b36a2e30eb2ca6b148088c6116848faa2
Florian <12160@holbertonstudents.com>
2026-04-21 16:21:58 +0200

    Refactor insert_school function docstring for clarity and conciseness

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,7 +3,5 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """Insert a new school document into the collection.
-    """
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    """Create a function that insert a new document in a collection"""
+    return mongo_collection.insert_one(kwargs).inserted_id
