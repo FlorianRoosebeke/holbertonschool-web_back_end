78e6c7ea1283a46352cc9225f619c0cb64279b95
Florian <12160@holbertonstudents.com>
2026-04-21 16:23:07 +0200

    Add insert_school function to create a new document in MongoDB collection

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that inserts a new school document into a MongoDB collection."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Create a function that insert a new document in a collection"""
+    return mongo_collection.insert_one(kwargs).inserted_id
