3bdb638f61b35cf609da3d8739b95e0af52106e9
Florian <12160@holbertonstudents.com>
2026-04-21 16:09:45 +0200

    Replace insert_school function with update_topics to modify school document topics based on name

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -2,6 +2,9 @@
 """Module that lists all documents in a MongoDB collection."""
 
 
-def insert_school(mongo_collection, **kwargs):
-    """Create a function that insert a new document in a collection"""
-    return mongo_collection.insert_one(kwargs).inserted_id
+def update_topics(mongo_collection, name, topics):
+    """Change all topics of a school document based on the name."""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
