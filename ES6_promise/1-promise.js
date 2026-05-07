21354317003e3fec9a17cad799ac93478db0c081
Florian <12160@holbertonstudents.com>
2026-05-07 11:15:47 +0200

    Add getFullResponseFromAPI function to handle API response based on success status

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+import { resolve } from "node:dns";
+
+export default function getFullResponseFromAPI(success) {
+	if (success) {
+		return Promise.resolve({ status: 200, body: 'Success' });
+	} else {
+		return Promise.reject('The fake API is not working currently');
+	}
+}
