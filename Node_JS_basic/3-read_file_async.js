5829494ee55c40773b04c6ca8ad0fffff07ebbe5
Florian <12160@holbertonstudents.com>
2026-05-12 16:25:53 +0200

    feat: enhance countStudents function to return detailed report of students by field

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -20,13 +20,17 @@ async function countStudents(path) {
 
 		console.log(`Number of students: ${students.length}`);
 
+		let report = `Number of students: ${students.length}\n`;
+
 		for (const field in fields) {
 			if (Object.prototype.hasOwnProperty.call(fields, field)) {
-				console.log(
-					`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
-				);
+				const msg = `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+				console.log(msg);
+				report += `${msg}\n`;
 			}
 		}
+
+		return report;
 	} catch (err) {
 		throw new Error('Cannot load the database');
 	}
