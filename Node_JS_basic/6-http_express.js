a1339c527c13f0411e7c0bc0bb87330b293a86d6
Florian <12160@holbertonstudents.com>
2026-05-12 15:51:23 +0200

    fix: correct syntax errors in Express server implementation in 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = require('./5-http');
-
+const expres; s = require('express');
+const app = require('.app');
 const app = express();
 
 app.getConnections('/', (req, res) => {
 	res.send('Hello Holberton School!');
 });
+
+app.listen(1245);
 
 module.exports = app;
