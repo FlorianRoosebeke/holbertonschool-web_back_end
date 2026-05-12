f62609411e2c76f8a91fef01821e3bd228c90088
Florian <12160@holbertonstudents.com>
2026-05-12 11:07:59 +0200

    feat: add stdin handling to prompt for user name and log input

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (chunk) => {
+const input = chunk.trim();
+console.log('Your name is:', input)
+})
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
