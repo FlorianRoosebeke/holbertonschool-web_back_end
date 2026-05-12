d5b12262f790b7e8bd3798ae02212a05aa864b6e
Florian <12160@holbertonstudents.com>
2026-05-12 15:24:01 +0200

    fix: correct formatting and update server listen method in 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
 const http = require('node:http');
 
-const app = http.createServer((req, res) =>{
-res.writeHead(200, { 'Content-Type' : 'text/plain' });
-res.end('Hello Holberton School!');
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
-server.listen(1245, '127.0.0.1');
+app.listen(1245);
+
 module.exports = app;
