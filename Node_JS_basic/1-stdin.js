cab9edd09de628cd2e353eb18b92e1a9f015c4f0
Florian <12160@holbertonstudents.com>
2026-05-12 11:10:19 +0200

    fix: correct indentation and ensure newline at end of file in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,10 +4,10 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (chunk) => {
-  const input = chunk.trim();
-  console.log('Your name is:', input);
+	const input = chunk.trim();
+	console.log('Your name is:', input);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+	console.log('This important software is now closing');
 });
