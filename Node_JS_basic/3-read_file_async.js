5b6b6720908629917255b3f12713e2421c3b5667
Florian <12160@holbertonstudents.com>
2026-05-12 14:47:14 +0200

    fix: update fs import to use promises for async file operations in countStudents function

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,33 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
 async function countStudents(path) {
-	try {
-		const data = await fs.readFile(path, 'utf-8');
-		const lines = data.split('\n').filter((line) => line.trim() !== '');
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-		const students = lines.slice(1);
-		const fields = {};
+    const students = lines.slice(1);
+    const fields = {};
 
-		for (const student of students) {
-			const [firstname, lastname, age, field] = student.split(',');
+    for (const student of students) {
+      const [firstname, lastname, age, field] = student.split(',');
 
-			if (!fields[field]) {
-				fields[field] = [];
-			}
+      if (!fields[field]) {
+        fields[field] = [];
+      }
 
-			fields[field].push(firstname);
-		}
+      fields[field].push(firstname);
+    }
 
-		console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students: ${students.length}`);
 
-		for (const field in fields) {
-			console.log(
-				`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
-			);
-		}
-	} catch (err) {
-		throw new Error('Cannot load the database');
-	}
-	resolve();
+    for (const field in fields) {
+      console.log(
+        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
