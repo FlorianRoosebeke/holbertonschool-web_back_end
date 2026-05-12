34617e43695a9963aa9dfe0310699bd828135c20
Florian <12160@holbertonstudents.com>
2026-05-12 10:36:42 +0200

    refactor: simplify displayMessage function to log a single message

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,8 +1,3 @@
-export default function displayMessage() {
-
-	const args = process.argv.slice(2);
-	console.log('Arguments:', args);
-	args.forEach((arg, index) => {
-		console.log(`${index + 1}: ${arg}`);
-	});
+export default function displayMessage(message) {
+  console.log(message);
 }
