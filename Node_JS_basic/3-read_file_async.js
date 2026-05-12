47640dc9a43dc16e2ed66483a463afa9de633127
Florian <12160@holbertonstudents.com>
2026-05-12 14:43:50 +0200

    feat: add countStudents function to read and process student data from a CSV file

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+	try {
+		const data = await fs.readFile(path, 'utf-8');
+		const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+		const students = lines.slice(1);
+		const fields = {};
+
+		for (const student of students) {
+			const [firstname, lastname, age, field] = student.split(',');
+
+			if (!fields[field]) {
+				fields[field] = [];
+			}
+
+			fields[field].push(firstname);
+		}
+
+		console.log(`Number of students: ${students.length}`);
+
+		for (const field in fields) {
+			console.log(
+				`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+			);
+		}
+	} catch (err) {
+		throw new Error('Cannot load the database');
+	}
+}
+
+module.exports = countStudents;
